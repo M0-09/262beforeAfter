8e59fda05a3aa54e58649f7e4b4e0cae5e4f01e0
Moksham <mokshamsharma09@gmail.com>
2024-11-07 13:59:55 -0700

    finished all modifications

--- script.js
+++ script.js
@@ -7,26 +7,65 @@ const beforeNumber = 100;
 
 // decrement by 2
 let afterDecrement = beforeNumber - 2;
-console.log("after: ", afterDecrement);
+console.log("before: ", beforeNumber);
+console.log("after decrement: ", afterDecrement);
+console.log("\n");
 
 // convert to string
 afterToString = beforeNumber.toString();
-console.log("after: ", afterToString);
+console.log("before: ", beforeNumber);
+console.log("after converting to string: ", afterToString);
+console.log("\n");
 
 // check if finite
 let checkIfFinite = isFinite(beforeNumber);
-console.log("after: ", checkIfFinite);
+console.log("before: ", beforeNumber);
+console.log("finite?: ", checkIfFinite);
+console.log("\n");
 
 // check if integer
 let checkIfInteger = Number.isInteger(beforeNumber);
-console.log("after: ", checkIfInteger);
+console.log("before: ", beforeNumber);
+console.log("if integer?: ", checkIfInteger);
+console.log("\n");
+
+// check if Not a number
+let checkIfNaN = isNaN(beforeNumber);
+console.log("before: ", beforeNumber);
+console.log("after checking for NaN: ", checkIfNaN);
+console.log("\n");
 
 // MODIFICATIONS- String
 
 // find length
 let stringLength = beforeString.length;
-console.log("after: ", stringLength);
+console.log("before: ", beforeString);
+console.log("String length: ", stringLength);
+console.log("\n");
 
 // replace parts of string
 let newString = beforeString.replace("Hello", "Goodbye");
-console.log("after: ", newString);
+console.log("before: ", beforeString);
+console.log("after replacing: ", newString);
+console.log("\n");
+
+// access the first character
+let firstChar = beforeString[0];
+console.log("before: ", beforeString);
+console.log("first character: ", firstChar);
+console.log("\n");
+
+// access the last character
+let lastChar = beforeString[beforeString.length - 1];
+console.log("before: ", beforeString);
+console.log("last character: ", lastChar);
+console.log("\n");
+
+// check start
+let checkStart = beforeString.startsWith("world");
+console.log("before: ", beforeString);
+console.log(
+  'after checking start for the first word to be "world": ',
+  checkStart
+);
+console.log("\n");
