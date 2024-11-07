c8596404fd28dd73a7650ae4573add7c2e31b1ad
Moksham <mokshamsharma09@gmail.com>
2024-11-07 13:15:40 -0700

    added more modifications

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-// before values
+// BEFORE VALUES
 const beforeString = "Hello, World!";
 
 const beforeNumber = 100;
 
-// modifications
+// MODIFICATIONS- Number
+
+// decrement by 2
 let afterDecrement = beforeNumber - 2;
-console.log(afterDecrement);
+console.log("after: ", afterDecrement);
+
+// convert to string
+afterToString = beforeNumber.toString();
+console.log("after: ", afterToString);
+
+// check if finite
+let checkIfFinite = isFinite(beforeNumber);
+console.log("after: ", checkIfFinite);
+
+// check if integer
+let checkIfInteger = Number.isInteger(beforeNumber);
+console.log("after: ", checkIfInteger);
+
+// MODIFICATIONS- String
+
+// find length
+let stringLength = beforeString.length;
+console.log("after: ", stringLength);
+
+// replace parts of string
+let newString = beforeString.replace("Hello", "Goodbye");
+console.log("after: ", newString);
